a4ffc00859f6708eb1f6a8803179d2ee8ce9decb
Yanis Dahmane <yanis.dahmane.pro@gmail.com>
2022-11-16 12:16:38 +0100

    finish

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :restaurants do
+    resources :reviews, only: %i[create]
+  end
+  resources :reviews, only: %i[show edit update destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
